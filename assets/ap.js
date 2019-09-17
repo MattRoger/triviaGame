59b3b826afc269845e142f6341e0b5e3f9cc06bd
MattRoger <mrogerphotography@gmail.com>
2019-09-16 20:52:14 -0600

    nothing accomplished but i have to turn something in

--- assets/ap.js
+++ assets/ap.js
@@ -1,70 +1,61 @@
-var possibleAnswers = [];
-var questions="";
-var correctAnswer=false;
-var score=0;
-var correctGuess=0;
-var wrongGuess=0;
+var answers = {
+    correct: 0,
+    incorrect: 0
+};
+var options = [{
+    question: "In Aladdin, what is the name of Jasmine's pet tiger?",
+    choices: ["Rajah", "Bo", "Iago", "Jack"],
+    correct: 0
+}, {
+    question: "In Peter Pan, Captain Hook had a hook on which part of his     body?",
+    choices: ["Right Foot", "Left Hand", "Left Foot", "Right Hand"],
+    correct: 1
+
+}, {
+    question: "In the Lion King, where does Mufasa and his family live?",
+    choices: ["Rocky Mountain", "Forest", "Desert", "Pride Rock"],
+    correct: 3
+
+}, {
+    question: "In Beauty and the Beast, how many eggs does Gaston eat for    breakfast?",
+    choices: ["2 Dozen", "5 Dozen", "5000", "0"],
+    correct: 1
+
+}, {
+    question: "In Alice in Wonderland, what is the name of Alice’s kitten?",
+    choices: ["Dinah", "Sammie", "Kat", "Luna"],
+    correct: 0
+
+}, {
+    question: "After being on earth, where did Hercules first meet his   father Zeus?",
+    choices: ["Mount Olympus", "Greece", "In the Temple of Zeus", "Elysian   Fields"],
+    correct: 2
+
+}, {
+    question: "During the ballroom scene of Beauty & the Beast, what color is Belle’s Gown?",
+    choices: ["Yellow", "Blue", "Gold", "White"],
+    correct: 2
+
+}, {
+    question: "In Bambi, what word does the owl use to describe falling in love?",
+    choices: ["Whimsical", "Miserable", "Joyful", "Twitterpatted"],
+    correct: 3
+}]
+console.log(this.question);
+
+// function ask ask question and choices
+function ask() {
 
 
 
-
-$(document).ready(function () {
-    $("#score").text("Score:" + score);
-    $("#correct-guess").text("Correct Answers: "+ correctGuess);
-    $("#wrong-guess").text("Wrong Answers: " +wrongGuess )
-
-    $("#start").html("<button> start");
-
-    $("#start").on("click", function (){
-
-
+    for (var i =0; i < options.length; i++) {
         
-        
-        var question= "Who is Luke's droid?";
-        var possibleAnswers = [
-            "c3po", "r2d2", "bb-8", "t1b9"
-    ]
-    var answer= possibleAnswers[1];
-    
-    for(var i=0;i<possibleAnswers.length;i++){
-        var button=$(`<button>`);
-        button.text(possibleAnswers[i]);
-        button.attr("data-answerchoice", i);
-        $("#possible-answers").append(button);
+        $("#question_div").text(options[i].question);
+       
+        $("#choices_div").html("<button>" + options[i].choices);
+
     }
-    $("#possible-answers").on("click" ,"button", function(){
-        var userPick = $(this).data("answerchoice")
-        console.log(userPick)
-        if(userPick==1){
-            correctAnswer=true;
-            score + 10;
-            correctGuess++;
-            console.log("win")
-        }else
-        { wrongGuess++;
-            console.log("loser")
-        }
-        
-        
-    })
-    
-    
-    printQ(question);
-
-})
-
-})
-
-
-
-
-
-
-
-
-
-function printQ(x) {
-    $("#question").text(x);
 }
-
-
+$("#start_button").on("click", function () {
+    ask()
+})
